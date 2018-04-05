fccecb791f8949e317c30a43ed8b0e9446c86c41
PlugInPlaya <tsanders1114@gmail.com>
2018-04-04 22:31:41 -0400

    Adding Wrapper

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Superheros from "./Superheros.json";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+         <Wrapper>
+                <Title>To boldly click where no one has clicked before!</Title>
+        
+                <h3 className="scoreSummary">
+                    {this.state.clickMessage}
+                </h3>
+                
+                <h3 className="scoreSummary">
+                    Correct Guesses: {this.state.correctGuesses} 
+                    <br />
+                    Best Score: {this.state.bestScore} 
+                </h3>
+
+                {this.state.superheros.map(superhero => (
+                    <superhero
+                        setClicked={this.setClicked}
+                        id={superhero.id}
+                        key={superhero.id}
+                        image={superhero.image}
+                    />
+                ))}
+            </Wrapper>
     );
   }
 }
